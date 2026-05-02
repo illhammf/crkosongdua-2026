3389a25a90306eb032d63ae73510b2bef7f650a6
illhammf <ilhamfrafli@gmail.com>
2026-05-02 19:31:23 +0700

    update tampilan css versi Ilham

--- src/resources/views/components/banner.blade.php
+++ src/resources/views/components/banner.blade.php
@@ -5,45 +5,47 @@
           <div class="owl-carousel owl-banner">
             <div class="item item-1">
               <div class="header-text">
-                <span class="category">Our Courses</span>
-                <h2>With Scholar Teachers, Everything Is Easier</h2>
-                <p>Ini adalah Proyek Ilham Firmansyah (20240801102).</p>
+                <span class="category">Assalamualaikum</span>
+                <h2>Hai Ganteng, Selamat Datang Ya Semoga Semangat dan Sehat Selalu 🤩</h2>
+                <p>Ini adalah Latihan Proyek PEMWEB Ilham Firmansyah (20240801102) dan semoga ini menjadikanmu lebih baik!</p>
                 <div class="buttons">
                   <div class="main-button">
-                    <a href="#">Request Demo</a>
+                    <a href="#">Mulai</a>
                   </div>
                   <div class="icon-button">
-                    <a href="#"><i class="fa fa-play"></i> What's Scholar?</a>
+                    <a href="#"><i class="fa fa-play"></i> Siapa Ilham?</a>
                   </div>
                 </div>
               </div>
             </div>
+            
             <div class="item item-2">
               <div class="header-text">
-                <span class="category">Best Result</span>
-                <h2>Get the best result out of your effort</h2>
-                <p>You are allowed to use this template for any educational or commercial purpose. You are not allowed to re-distribute the template ZIP file on any other website.</p>
+                <span class="category">Semangat Bareng Ilham</span>
+                <h2>Kalau Ilham Aja Semangat, Masa Kamu Enggak? 😆</h2>
+                <p>Tenang aja, semua butuh proses. Bahkan Ilham juga pernah bingung, tapi tetap lanjut belajar biar makin keren!</p>
                 <div class="buttons">
                   <div class="main-button">
-                    <a href="#">Request Demo</a>
+                    <a href="#">Gas Belajar</a>
                   </div>
                   <div class="icon-button">
-                    <a href="#"><i class="fa fa-play"></i> What's the best result?</a>
+                    <a href="#"><i class="fa fa-play"></i> Ilham Ngapain Aja?</a>
                   </div>
                 </div>
               </div>
             </div>
+
             <div class="item item-3">
               <div class="header-text">
-                <span class="category">Online Learning</span>
-                <h2>Online Learning helps you save the time</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporious incididunt ut labore et dolore magna aliqua suspendisse.</p>
+                <span class="category">Belajar Santai</span>
+                <h2>Belajar Online: Bisa Sambil Rebahan Kayak Ilham 😴</h2>
+                <p>Nggak harus tegang, belajar juga bisa santai. Yang penting tetap paham, bukan cuma kelihatan sibuk doang hehe.</p>
                 <div class="buttons">
                   <div class="main-button">
-                    <a href="#">Request Demo</a>
+                    <a href="#">Coba Yuk</a>
                   </div>
                   <div class="icon-button">
-                    <a href="#"><i class="fa fa-play"></i> What's Online Course?</a>
+                    <a href="#"><i class="fa fa-play"></i> Rahasia Ilham?</a>
                   </div>
                 </div>
               </div>
